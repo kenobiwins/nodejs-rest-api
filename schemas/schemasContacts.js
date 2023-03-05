dd2eb6a687e23a27330c633bad71a789af27637a
Max Sidorenko <fivemoshr@gmail.com>
2023-03-05 22:21:34 +0200

    Update schemasContacts.js

--- schemas/schemasContacts.js
+++ schemas/schemasContacts.js
@@ -1,10 +1,27 @@
 const Joi = require("joi");
 
 const addContactSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
+  name: Joi.string()
+    .required()
+    .messages({ "any.required": `missing required name field` }),
+  email: Joi.string()
+    .required()
+    .messages({ "any.required": `missing required email field` }),
+  phone: Joi.string()
+    .required()
+    .messages({ "any.required": `missing required phone field` }),
 });
+// .error((errors) => {
+// errors.forEach((error) => {
+//   switch (error.type) {
+//     case value:
+//       break;
+
+//     default:
+//       break;
+//   }
+// });
+// });
 
 const updateContactSchema = Joi.object({
   name: Joi.string().optional(),
