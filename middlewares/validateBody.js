979bd881929108ab89d56d62d7694168330f754d
Max Sidorenko <fivemoshr@gmail.com>
2023-03-05 21:52:55 +0200

    update

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -4,7 +4,7 @@ const validateBody = (schema, message = "missing fields") => {
   const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
     if (error) {
-      return next(httpError(404, message));
+      return next(httpError(400, error.message));
     }
     next();
   };
